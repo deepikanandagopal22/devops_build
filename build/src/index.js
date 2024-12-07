9bceb7520e680dd15b19d4a0d411f98f2d12b137
deepikanandagopal22 <deepikanandagopal1998@gmail.com>
2024-12-07 10:36:19 +0530

    Create index.js

---
+++ build/src/index.js
@@ -0,0 +1,14 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./static/css/main.cf5c13c5.css"; // Import your main CSS file
+import App from "./App"; // Import the main App component
+import reportWebVitals from "./reportWebVitals";
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>
+);
+
+reportWebVitals();
