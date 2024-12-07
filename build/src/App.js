82a19bf8c90e6693406ffa29e638f07c72fc7bf1
deepikanandagopal22 <deepikanandagopal1998@gmail.com>
2024-12-07 10:36:41 +0530

    Create App.js

---
+++ build/src/App.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+const App = () => {
+  return (
+    <div className="App">
+      <h1>Welcome to My Static Web App</h1>
+      <p>This is a sample React application served using Docker and Nginx.</p>
+    </div>
+  );
+};
+
+export default App;
